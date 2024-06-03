45ac583ddf4a40230fecd7dace99d7351680e001
DragonChen <teacher144123@gmail.com>
2024-06-03 09:58:58 +0800

    add OPENAI package and test code

--- src/main.py
+++ src/main.py
@@ -1,4 +1,24 @@
-from at_translate import main
+from openai import OpenAI
+from dotenv import load_dotenv
+import logging
 
-if __name__ == "__main__":
-    main()
+status = load_dotenv()
+logging.info(f"Status of loading .env file: {status}")
+client = OpenAI()
+
+msgs = [
+    {"role": "system", "content": "You are a helpful assistant."},
+    {"role": "user", "content": "Who won the world series in 2020?"},
+    {
+        "role": "assistant",
+        "content": "The Los Angeles Dodgers won the World Series in 2020.",
+    },
+    {"role": "user", "content": "Where was it played?"},
+]
+
+total_len = sum([len(m['content']) for m in msgs])
+print('total_len:', total_len, 'tokens: 53')
+
+
+# response = client.chat.completions.create(model="gpt-3.5-turbo", messages=msgs)
+# print(response)
